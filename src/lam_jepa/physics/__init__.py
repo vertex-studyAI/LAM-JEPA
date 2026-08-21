c562415a5254856c23a25dc076aa4489b307a2dc
build-the-future-11 <ryangomez.hs@gmail.com>
2026-08-21 18:36:30 +0530

    research: export PHY-JEPA phase-0 utilities

--- src/lam_jepa/physics/__init__.py
+++ src/lam_jepa/physics/__init__.py
@@ -4,11 +4,25 @@ These utilities are intentionally independent from historical ARC experiment cod
 """
 
 from .masking import make_observation_mask
+from .phase0 import (
+    LinearProbe,
+    OscillatorPhase0Config,
+    fit_linear_probe,
+    generate_oscillator_phase0_dataset,
+    median_normalized_parameter_error,
+    split_indices_from_manifest,
+)
 from .simulators import DampedOscillatorParams, simulate_damped_oscillator, split_parameter_grid
 
 __all__ = [
     "DampedOscillatorParams",
+    "LinearProbe",
+    "OscillatorPhase0Config",
+    "fit_linear_probe",
+    "generate_oscillator_phase0_dataset",
     "make_observation_mask",
+    "median_normalized_parameter_error",
     "simulate_damped_oscillator",
+    "split_indices_from_manifest",
     "split_parameter_grid",
 ]
